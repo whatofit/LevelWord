24218a8b3ca161fab60164e6d923a307cc8a15f2
whatofit <whatofit@163.com>
2017-06-20 00:01:12 +0800

    实现合并单元格demo

---
+++ src/com/mergetablecells/ICellFont.java
@@ -0,0 +1,23 @@
+package com.mergetablecells;
+
+//package jtable.mergecells3;
+//
+//import java.awt.Font;
+//
+///*
+// * (swing1.1beta3)
+// */
+//
+///**
+// * @version 1.0 11/22/98
+// */
+//
+//interface ICellFont {
+//
+//    public Font getFont(int row, int column);
+//
+//    public void setFont(Font font, int row, int column);
+//
+//    public void setFont(Font font, int[] rows, int[] columns);
+//
+// }
